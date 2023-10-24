db30f3297712b85dc82c33c37e1ac28da072150d
Renny Mwangi <rennymwangi@gmail.com>
2023-10-24 21:22:50 +1030

    Defined jwt, login and register user

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,51 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    users: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+  },
+  Mutation: {
+    registerUser: async (parent, args) => {
+      const { username, email, password } = args.registerInput;
+      // see if an old user exists with email attempting to register
+      const oldUser = await User.findOne({ email: args.email });
+
+      // Throw error if that user exists
+      if (oldUser) {
+        throw new Error('User already exists');
+      }
+      // Create new user
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    loginUser: async (parent, args) => {
+      const { email, password } = args.loginInput;
+      const user = await User.findOne({ email: email });
+
+      console.log('User', user);
+      if (!user) {
+        throw new Error('User not found');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new Error('Incorrect password');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
